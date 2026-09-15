1f999d4f4ef5e20e837ec23a8e9c37efa56e39c8
Николай Веселов ТКИ-142 <vnm1705@mail.ru>
2026-09-15 21:07:25 +0300

    Update ConstantGenerator.h

--- ConstantGenerator.h
+++ ConstantGenerator.h
@@ -1,11 +1,25 @@
 #pragma once
 #include "Generator.h"
+
 namespace miit::algebra {
-class ConstantGenerator final : public Generator {
-private:
-    int value_;
-public:
-    explicit ConstantGenerator(int value);
-    int generate() override;
-};
+    /**
+     * @brief Генератор, возвращающий одно и то же константное значение
+     */
+    class ConstantGenerator final : public Generator {
+    private:
+        int value_; /**< Константное значение, которое возвращает генератор */
+
+    public:
+        /**
+         * @brief Конструктор через константное значение
+         * @param value - значение, которое будет возвращать генератор
+         */
+        explicit ConstantGenerator(int value);
+
+        /**
+         * @brief Возвращает константное значение
+         * @return константное значение
+         */
+        int generate() override;
+    };
 }

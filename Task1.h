2ba23a6d3df4ec0caedb245d36f837b0606b1a81
Николай Веселов ТКИ-142 <vnm1705@mail.ru>
2026-09-17 15:09:34 +0300

    Update Task1.h

--- Task1.h
+++ Task1.h
@@ -1,10 +1,20 @@
-﻿#pragma once
+#pragma once
 #include "Exercise.h"
 
 namespace miit::algebra {
+    /**
+     * @brief Первое задание варианта 3: замена элементов первых трех столбцов на их квадраты
+     */
     class Task1 final : public Exercise {
     public:
+        /**
+         * @brief Наследует конструкторы базового класса Exercise
+         */
         using Exercise::Exercise;
+
+        /**
+         * @brief Выполняет задание над матрицей
+         */
         void solve() override;
     };
 }

994ca6588bd25066ab213b4ea77b91aaff5e3f59
Николай Веселов ТКИ-142 <vnm1705@mail.ru>
2026-09-17 15:09:49 +0300

    Update Task2.h

--- Task2.h
+++ Task2.h
@@ -1,10 +1,20 @@
-﻿#pragma once
+#pragma once
 #include "Exercise.h"
 
 namespace miit::algebra {
+    /**
+     * @brief Второе задание варианта 3: вставка после каждой нечетной строки первой строки
+     */
     class Task2 final : public Exercise {
     public:
+        /**
+         * @brief Наследует конструкторы базового класса Exercise
+         */
         using Exercise::Exercise;
+
+        /**
+         * @brief Выполняет задание над матрицей
+         */
         void solve() override;
     };
 }

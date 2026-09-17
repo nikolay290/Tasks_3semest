e708e7dbe1f3b8b6925f0c291e932d63aba46708
Николай Веселов ТКИ-142 <vnm1705@mail.ru>
2026-09-17 15:07:55 +0300

    Update IStreamGenerator.h

--- IStreamGenerator.h
+++ IStreamGenerator.h
@@ -1,12 +1,26 @@
 #pragma once
 #include "Generator.h"
 #include <istream>
+
 namespace miit::algebra {
-class IStreamGenerator final : public Generator {
-private:
-    std::istream& input_;
-public:
-    explicit IStreamGenerator(std::istream& input);
-    int generate() override;
-};
+    /**
+     * @brief Генератор, читающий значения из входного потока
+     */
+    class IStreamGenerator final : public Generator {
+    private:
+        std::istream& input_; /**< Ссылка на входной поток */
+
+    public:
+        /**
+         * @brief Конструктор через входной поток
+         * @param input - входной поток для чтения значений
+         */
+        explicit IStreamGenerator(std::istream& input);
+
+        /**
+         * @brief Читает очередное значение из потока
+         * @return считанное значение
+         */
+        int generate() override;
+    };
 }

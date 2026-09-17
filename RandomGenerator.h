929b1932a4aba994c70d0d7fd26b56ac8a0b8bd6
Николай Веселов ТКИ-142 <vnm1705@mail.ru>
2026-09-17 15:09:05 +0300

    Update RandomGenerator.h

--- RandomGenerator.h
+++ RandomGenerator.h
@@ -1,13 +1,28 @@
 #pragma once
 #include "Generator.h"
 #include <random>
+
 namespace miit::algebra {
-class RandomGenerator final : public Generator {
-private:
-    std::mt19937 engine_;
-    std::uniform_int_distribution<int> distribution_;
-public:
-    RandomGenerator(int minimum, int maximum);
-    int generate() override;
-};
+    /**
+     * @brief Генератор случайных целых чисел в заданном диапазоне
+     */
+    class RandomGenerator final : public Generator {
+    private:
+        std::mt19937 engine_;                            /**< Генератор псевдослучайных чисел Mersenne Twister */
+        std::uniform_int_distribution<int> distribution_; /**< Равномерное распределение целых чисел */
+
+    public:
+        /**
+         * @brief Конструктор через границы диапазона
+         * @param minimum - нижняя граница диапазона
+         * @param maximum - верхняя граница диапазона
+         */
+        RandomGenerator(int minimum, int maximum);
+
+        /**
+         * @brief Генерирует случайное число в заданном диапазоне
+         * @return случайное число
+         */
+        int generate() override;
+    };
 }

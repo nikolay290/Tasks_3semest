f9ac9027bd88f94ddc57ce34c3d2957b079fdd2e
Николай Веселов ТКИ-142 <vnm1705@mail.ru>
2026-09-22 19:03:19 +0300

    Delete RandomGenerator.cpp

--- RandomGenerator.cpp
+++
@@ -1,9 +0,0 @@
-#include "RandomGenerator.h"
-#include <stdexcept>
-namespace miit::algebra {
-RandomGenerator::RandomGenerator(int minimum, int maximum)
-    : engine_(std::random_device{}()), distribution_(minimum, maximum) {
-    if (minimum > maximum) throw std::invalid_argument("Minimum must not exceed maximum");
-}
-int RandomGenerator::generate() { return distribution_(engine_); }
-}

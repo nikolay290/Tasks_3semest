2885f69267ddb14039a095aa0315f69e30637297
Николай Веселов ТКИ-142 <vnm1705@mail.ru>
2026-09-22 18:59:08 +0300

    Delete ConstantGenerator.cpp

--- ConstantGenerator.cpp
+++
@@ -1,5 +0,0 @@
-﻿#include "D:\ConstantGenerator.h"
-namespace miit::algebra {
-ConstantGenerator::ConstantGenerator(int value) : value_(value) {}
-int ConstantGenerator::generate() { return value_; }
-}
